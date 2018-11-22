435b78f83e3eaee8cab17aa76256d98ecb26503f
Jian <jianqilu1126@gmail.com>
2018-11-22 02:03:13 +0000

    save

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -3,6 +3,6 @@
 @include('_includes.top.header')
 <p class="flow-text">Successful Updated!</p>
 
-<a href="/print" class="btn waves-effect waves-light">Go Print</a>
+{{-- <a href="/print" class="btn waves-effect waves-light">Go Print</a> --}}
 
 @endsection 
